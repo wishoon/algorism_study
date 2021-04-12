e3022adf052dfc14cef3725e42195b96c9bb2596
Wishoon <heonga2@gmail.com>
2021-04-12 21:06:26 +0900

    프로그래머스 124 나라의 숫자

---
+++ src/main/java/v2/Programmers/프124_나라의_숫자.java
@@ -0,0 +1,26 @@
+package v2.Programmers;
+
+public class 프124_나라의_숫자 {
+    public static void main(String[] args) {
+
+        solution(513);
+    }
+
+    public static String solution(int n) {
+        String[] numbers = {"4", "1", "2"};
+        String answer = "";
+
+        int num = n;
+
+        while(num > 0){
+            int remainder = num % 3;
+            num /= 3;
+
+            if(remainder == 0) num--;
+
+            answer = numbers[remainder] + answer;
+        }
+
+        return answer;
+    }
+}
