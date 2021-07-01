8d73f37782ed9e0e448453a6664ea46c63b013cd
wishoon <heonga2@gmail.com>
2021-07-01 21:28:36 +0900

    2021.07.01

---
+++ src/main/java/Inflearn/문자_찾기.java
@@ -0,0 +1,40 @@
+package Inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문자_찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        solution(str, c);
+    }
+
+    public static int solution(String str, char c) {
+        int answer = 0;
+
+        str = str.toUpperCase();    // string이 다 대문자로 변경
+        c = Character.toUpperCase(c);   // char을 대문자로 변경
+
+        // 방법 1.
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        answer = 0;
+        // 방법 2.
+        for (char i : str.toCharArray()) {
+            if(i == c) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
---
+++ src/main/java/Inflearn/회문문자열.java
@@ -0,0 +1,27 @@
+package Inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 회문문자열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        str = str.toUpperCase();
+
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+}
