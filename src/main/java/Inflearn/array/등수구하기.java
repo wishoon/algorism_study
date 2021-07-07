ae685f44457834d7048024698a8005db46789fa3
wishoon <heonga2@gmail.com>
2021-07-07 22:32:30 +0900

    2021.07.07

---
+++ src/main/java/Inflearn/array/등수구하기.java
@@ -0,0 +1,41 @@
+package Inflearn.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 등수구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String input = br.readLine();
+
+        solution(n, input);
+    }
+
+    public static void solution(int n, String input) {
+        String[] arr = input.split(" ");
+
+        int[] score = new int[arr.length];
+
+        Arrays.fill(score, 1);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(i == j)
+                    continue;
+
+                if(Integer.parseInt(arr[i]) < Integer.parseInt(arr[j])) {
+                    score[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(score[i]);
+        }
+    }
+}
