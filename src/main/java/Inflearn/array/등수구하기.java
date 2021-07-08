501672b1cc6bfec90754a2e0aa1bef3ef8bd2073
wishoon <heonga2@gmail.com>
2021-07-08 22:19:08 +0900

    2021.07.08

--- src/main/java/Inflearn/array/등수구하기.java
+++ src/main/java/Inflearn/array/등수구하기.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class 등수구하기 {
     public static void main(String[] args) throws IOException {
