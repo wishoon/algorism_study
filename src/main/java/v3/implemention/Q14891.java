2bc2178f6bb84ee8dad569c87bccd0baf18e5390
WishooN <48710213+Wishoon@users.noreply.github.com>
2021-11-19 21:10:51 +0900

    2021.11.19

--- src/main/java/v3/implemention/Q14891.java
+++ src/main/java/v3/implemention/Q14891.java
@@ -24,7 +24,8 @@ public class Q14891 {
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            rotate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), true, true);
+//            rotate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), true, true);
+            rotate2(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
         int answer = 0;
@@ -36,7 +37,53 @@ public class Q14891 {
 
         System.out.println(answer);
     }
+    private static void rotate2(int cur_idx, int dir) {
 
+        int[] check = new int[5];
+        check[cur_idx] = dir;
+
+        int stand_dir = dir;
+        int stand_idx = cur_idx;
+
+        boolean left_flag = true;
+        int left_idx = cur_idx;
+
+        while (left_flag) {
+            left_idx -= 1;
+
+            if (left_idx >= 1 && wheel[left_idx].charAt(2) != wheel[stand_idx].charAt(6)) {
+                check[left_idx] = -stand_dir;
+
+                stand_idx = left_idx;
+                stand_dir = -stand_dir;
+            } else {
+                left_flag = false;
+            }
+        }
+
+        stand_dir = dir;
+        stand_idx = cur_idx;
+
+        boolean right_flag = true;
+        int right_idx = cur_idx;
+
+        while (right_flag) {
+            right_idx += 1;
+
+            if (right_idx <= 4 && wheel[right_idx].charAt(6) != wheel[stand_idx].charAt(2)) {
+                check[right_idx] = -stand_dir;
+
+                stand_idx = right_idx;
+                stand_dir = -stand_dir;
+            } else {
+                right_flag = false;
+            }
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            wheel[i] = change(wheel[i], check[i]);
+        }
+    }
 
     private static void rotate(int cur_idx, int dir, boolean left, boolean right) {
         int left_idx = cur_idx - 1;
@@ -55,6 +102,9 @@ public class Q14891 {
     }
 
     public static String change(String state, int dir) {
+        if (dir == 0) {
+            return state;
+        }
         if (dir == 1) {
             return state.charAt(state.length() - 1) + state.substring(0, state.length() - 1);
         } else {
